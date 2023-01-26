d463d29cbe1e980e89c5953ba62b4a69a833d344
jthomas-rsi <112565472+jthomas-rsi@users.noreply.github.com>
2023-01-25 19:02:53 -0600

    FirstForm styled (#5)

--- src/components/FirstForm.tsx
+++ src/components/FirstForm.tsx
@@ -1,13 +1,16 @@
-import { useForm, Controller } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import Stack from "@mui/material/Stack";
 import SendIcon from "@mui/icons-material/Send";
 import Slider from "@mui/material/Slider";
 import TextField, { TextFieldProps } from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 import InputLabel from "@mui/material/InputLabel";
 
 // create interface for from props
@@ -39,135 +42,164 @@ const FirstForm = () => {
   return (
     <Box
       sx={{
-        marginTop: "5em",
+        minHeight: "100vh",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        backgroundColor: "#588157",
       }}
     >
-      <form onSubmit={handleSubmit(handleFormSubmit)}>
-        <Stack spacing={2}>
-          {/* First name */}
-          <Controller
-            name="firstName"
-            control={control}
-            defaultValue=""
-            render={({ field: { onChange, value }, fieldState: { error } }) => (
-              <TextField
-                required
-                label="First Name"
-                variant="outlined"
-                value={value}
-                onChange={onChange}
-                error={!!error}
-                helperText={error ? error.message : null}
+      <Card
+        sx={{
+          backgroundColor: "#DAD7CD",
+        }}
+      >
+        <CardContent>
+          <Typography variant="h6" mb={2}>
+            Client Data
+          </Typography>
+          <form onSubmit={handleSubmit(handleFormSubmit)}>
+            <Stack spacing={2}>
+              {/* First name */}
+              <Controller
+                name="firstName"
+                control={control}
+                defaultValue=""
+                render={({
+                  field: { onChange, value },
+                  fieldState: { error },
+                }) => (
+                  <TextField
+                    required
+                    label="First Name"
+                    variant="outlined"
+                    value={value}
+                    onChange={onChange}
+                    error={!!error}
+                    helperText={error ? error.message : null}
+                  />
+                )}
+                rules={{ required: "First name is required" }}
               />
-            )}
-            rules={{ required: "First name is requried" }}
-          />
 
-          {/* Last name */}
-          <Controller
-            name="lastName"
-            control={control}
-            defaultValue=""
-            render={({ field: { onChange, value }, fieldState: { error } }) => (
-              <TextField
-                required
-                label="Last name"
-                variant="outlined"
-                value={value}
-                onChange={onChange}
-                error={!!error}
-                helperText={error ? error.message : null}
+              {/* Last name */}
+              <Controller
+                name="lastName"
+                control={control}
+                defaultValue=""
+                render={({
+                  field: { onChange, value },
+                  fieldState: { error },
+                }) => (
+                  <TextField
+                    required
+                    label="Last name"
+                    variant="outlined"
+                    value={value}
+                    onChange={onChange}
+                    error={!!error}
+                    helperText={error ? error.message : null}
+                  />
+                )}
+                rules={{ required: "Last names is required" }}
               />
-            )}
-            rules={{ required: "Last names is requried" }}
-          />
 
-          {/* Age Group*/}
-          {/* TODO figure out a way to get value from slider comp */}
-          <Controller
-            name="age"
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <>
-                <InputLabel id="ageSelect-label">{"Age"}</InputLabel>
-                <Slider
-                  size="small"
-                  min={0}
-                  max={100}
-                  onChange={onChange}
-                  aria-label="age picker"
-                  valueLabelDisplay="auto"
+              {/* Age Group*/}
+              {/* TODO figure out a way to get value from slider comp */}
+              <Controller
+                name="age"
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <>
+                    <InputLabel id="ageSelect-label">{"Age"}</InputLabel>
+                    <Slider
+                      size="small"
+                      min={0}
+                      max={100}
+                      onChange={onChange}
+                      aria-label="age picker"
+                      valueLabelDisplay="auto"
+                    />
+                  </>
+                )}
+              />
+
+              {/* Date Of birth */}
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <Controller
+                  name={"birthDate"}
+                  control={control}
+                  render={({ field: { onChange, value } }) => (
+                    <DatePicker
+                      label="Date Of Birth"
+                      value={value}
+                      onChange={onChange}
+                      renderInput={(
+                        params: JSX.IntrinsicAttributes & TextFieldProps
+                      ) => <TextField {...params} />}
+                    />
+                  )}
                 />
-              </>
-            )}
-          />
+              </LocalizationProvider>
 
-          {/* Date Of birth */}
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <Controller
-              name={"birthDate"}
-              control={control}
-              render={({ field: { onChange, value } }) => (
-                <DatePicker
-                  label="Date Of Birth"
-                  value={value}
-                  onChange={onChange}
-                  renderInput={(
-                    params: JSX.IntrinsicAttributes & TextFieldProps
-                  ) => <TextField {...params} />}
-                />
-              )}
-            />
-          </LocalizationProvider>
-
-          {/* Phone Number */}
-          {/* TODO: restrict phone number to input to numbers and (xxx)xxx-xxxx format */}
-          <Controller
-            name="phoneNumber"
-            control={control}
-            defaultValue=""
-            render={({ field: { onChange, value }, fieldState: { error } }) => (
-              <TextField
-                required
-                label="Phone Number"
-                variant="outlined"
-                value={value}
-                onChange={onChange}
-                error={!!error}
-                helperText={error ? error.message : null}
+              {/* Phone Number */}
+              {/* TODO: restrict phone number to input to numbers and (xxx)xxx-xxxx format */}
+              <Controller
+                name="phoneNumber"
+                control={control}
+                defaultValue=""
+                render={({
+                  field: { onChange, value },
+                  fieldState: { error },
+                }) => (
+                  <TextField
+                    required
+                    label="Phone Number"
+                    variant="outlined"
+                    value={value}
+                    onChange={onChange}
+                    error={!!error}
+                    helperText={error ? error.message : null}
+                  />
+                )}
+                rules={{ required: "Phone number is required" }}
               />
-            )}
-            rules={{ required: "Phone number is requried" }}
-          />
 
-          {/* Email Address */}
-          <Controller
-            name="email"
-            control={control}
-            defaultValue=""
-            render={({ field: { onChange, value }, fieldState: { error } }) => (
-              <TextField
-                required
-                label="Email"
-                variant="outlined"
-                value={value}
-                onChange={onChange}
-                error={!!error}
-                helperText={error ? error.message : null}
+              {/* Email Address */}
+              <Controller
+                name="email"
+                control={control}
+                defaultValue=""
+                render={({
+                  field: { onChange, value },
+                  fieldState: { error },
+                }) => (
+                  <TextField
+                    required
+                    label="Email"
+                    variant="outlined"
+                    value={value}
+                    onChange={onChange}
+                    error={!!error}
+                    helperText={error ? error.message : null}
+                  />
+                )}
+                rules={{ required: "Email is required" }}
               />
-            )}
-            rules={{ required: "Email is requried" }}
-          />
 
-          {/* Submit Button */}
-          <Button type="submit" variant="contained"  color='primary' endIcon={<SendIcon />}>
-            Submit
-          </Button>
-        </Stack>
-      </form>
+              {/* Submit Button */}
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                endIcon={<SendIcon />}
+              >
+                Submit
+              </Button>
+            </Stack>
+          </form>
+        </CardContent>
+      </Card>
     </Box>
   );
 };
